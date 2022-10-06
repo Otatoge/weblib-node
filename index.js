761154c9da3e4e21651f2ebf316a3df75811f682
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-10-06 17:51:16 +0900

    First Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+require('jsdom-global')();
+
+// Global definition is not possible to avoid global pollution! Sad.
+// Does anyone know of a solution?
+module.exports = {
+    /**
+     * Returns window
+     * @returns {Window & typeof globalThis}
+     */
+    getWindow: () => {
+        return window
+    },
+    /**
+     * Load the web library
+     * @param {String} lib path, URL, or library contents
+     * @returns {Promise<*>}
+     */
+    loadLibrary: (lib) => {
+        return new Promise(async (resolve, reject) => {
+            // Do the reading inside the function to avoid global contamination, ahh
+            const { parse } = require('node:path');
+            const url = require('node:url');
+            const { default: fetch } = require('node-fetch');
+            try {
+                const path = parse(lib);
+                const { readFileSync } = require('node:fs');
+                const data = readFileSync(path);
+                resolve((() => new Function(data)())());
+            } catch {
+                try {
+                    new url.URL(lib);
+                    try {
+                        const res = await fetch(lib);
+                        const data = await res.text();
+                        resolve((() => new Function(data)())());
+                    } catch (err) {
+                        try {
+                            // There is a possibility of a file URL
+                            const path = url.fileURLToPath(lib);
+                            const { readFileSync } = require('node:fs');
+                            const data = readFileSync(path);
+                            resolve((() => new Function(data)())());
+                        } catch {
+                            reject(err);
+                        }
+                    }
+                } catch {
+                    resolve((() => new Function(lib)())());
+                }
+            }
+        });
+    }
+}
