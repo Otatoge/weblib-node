8ec67d9398d923fa9120487b95fe4508285616e0
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-10-06 21:37:40 +0900

    fix bug

--- index.js
+++ index.js
@@ -14,43 +14,11 @@ require('jsdom-global')();
 
     /**
      * Load the web library
-     * @param {String} lib path, URL, or library contents
+     * @param {String} lib ibrary contents
      * @returns {Promise<*>}
      */
     const loadLib = (lib) => {
-        return new Promise(async (resolve, reject) => {
-            // Do the reading inside the function to avoid global contamination, ahh
-            const { parse } = require('node:path');
-            const url = require('node:url');
-            const { default: fetch } = require('node-fetch');
-            try {
-                const path = parse(lib);
-                const { readFileSync } = require('node:fs');
-                const data = readFileSync(path);
-                resolve((() => new Function(data)())());
-            } catch {
-                try {
-                    new url.URL(lib);
-                    try {
-                        const res = await fetch(lib);
-                        const data = await res.text();
-                        resolve((() => new Function(data)())());
-                    } catch (err) {
-                        try {
-                            // There is a possibility of a file URL
-                            const path = url.fileURLToPath(lib);
-                            const { readFileSync } = require('node:fs');
-                            const data = readFileSync(path);
-                            resolve((() => new Function(data)())());
-                        } catch {
-                            reject(err);
-                        }
-                    }
-                } catch {
-                    resolve((() => new Function(lib)())());
-                }
-            }
-        });
+        return (()=>new Function(lib)())();
     }
 
     module.exports = {getWindow, loadLib}
